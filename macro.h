7f0dd6b112b5a489a134b0ec12a0483c23d28344
Marco D.G <39568183+Marco-DG@users.noreply.github.com>
2023-04-15 19:26:09 +0200

    Delete macro.h

--- macro.h
+++
@@ -1,7 +0,0 @@
-#ifndef _DGLIB_MACRO_H
-#define _DGLIB_MACRO_H
-
-#define DGLIB_CONCAT_HELPER(prefix, suffix)     prefix##suffix
-#define DGLIB_CONCAT(prefix, suffix)            DGLIB_CONCAT_HELPER(prefix, suffix)
-
-#endif /* _DGLIB_MACRO_H */
